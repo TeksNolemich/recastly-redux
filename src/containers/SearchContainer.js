ec830c1f08c1a0fd72b11ec01f245543d1dd6b75
arthurtharp <atharp@uci.edu>
2018-08-15 20:44:52 -0700

    store complete

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
-
-//TODO: define a SearchContainer component which will hook up your action
-// dispatchers with your search component props.
-//HINT: use react-redux 'connect' method to generate a container component from
-//state and dispatch mappings.
-
-export default SearchContainer;
+var mapStateToProps = function(state, ownProps) {
+  return {
+  };
+};
+var mapDispatchToProps = (dispatch) => {
+  return {
+     handleSearchInputChange: (value) => {dispatch(handleSearchChange(value))}
+    // fetchData: (url) => dispatch(itemsFetchData(url))
+  }
+// //TODO: define a SearchContainer component which will hook up your action
+// // dispatchers with your search component props.
+// //HINT: use react-redux 'connect' method to generate a container component from
+// //state and dispatch mappings.
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Search)
--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,8 +2,11 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
-
+var VideoListContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
+​
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
 
