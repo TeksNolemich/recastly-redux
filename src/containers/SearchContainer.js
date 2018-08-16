da243f9e93f336ddffcf62065b01fadbf6bfbf65
arthurtharp <atharp@uci.edu>
2018-08-16 11:30:39 -0700

    Remove unneed state map

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,10 +2,7 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var mapStateToProps = function(state, ownProps) {
-  return {
-  };
-};
+
 var mapDispatchToProps = (dispatch) => {
   return {
      handleSearchInputChange: (value) => {dispatch(handleSearchChange(value))}
@@ -16,4 +13,4 @@ var mapDispatchToProps = (dispatch) => {
 // //HINT: use react-redux 'connect' method to generate a container component from
 // //state and dispatch mappings.
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Search)
+export default connect(null, mapDispatchToProps)(Search)
